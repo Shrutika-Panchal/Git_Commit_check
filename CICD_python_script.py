93a212dec2e036e205df22709d36c82fd8bf6700
igSpanser <panchalshrutika00@gmail.com>
2024-02-17 19:51:06 +0530

    Uploading commit check code to repo

---
+++ CICD_python_script.py
@@ -0,0 +1,36 @@
+import requests
+from datetime import datetime, timedelta
+
+def get_new_commits(username, repository, token, since):
+    url = f'https://api.github.com/repos/{username}/{repository}/commits'
+    headers = {'Authorization': f'token {token}'}
+    params = {'since': since}
+
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        commits = response.json()
+        return commits
+    else:
+        print(f"Failed to fetch commits. Status code: {response.status_code}")
+        return None
+
+def main():
+    username = 'igSpanser'
+    repository = 'HV_CICD_Project'
+    token = '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'
+    
+    # Set the datetime for the last check (e.g., one day ago)
+    since_datetime = (datetime.now() - timedelta(hours=240)).isoformat()
+
+    commits = get_new_commits(username, repository, token, since_datetime)
+
+    if commits:
+        print(f"New commits in {username}/{repository}:")
+        for commit in commits:
+            print(f"Commit: {commit['sha'][:7]} by {commit['commit']['author']['name']}: {commit['commit']['message']}")
+    else:
+        print("No new commits.")
+
+if __name__ == "__main__":
+    main()
