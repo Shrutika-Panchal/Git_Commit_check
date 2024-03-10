d199ac5e11fd5f6a4978dd26ef6587ec42f1b2e7
igSpanser <panchalshrutika00@gmail.com>
2024-03-10 12:02:10 +0530

    Adding code to repo

---
+++ Calculator.py
@@ -0,0 +1,34 @@
+while True:
+    # take input from the user
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        try:
+            a = print(input("Enter first number: "))
+            b = print(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(a, "+", b, "=", add(a, b))
+
+        elif choice == '2':
+            print(a, "-", b, "=", subtract(a, b))
+
+        elif choice == '3':
+            print(a, "*", b, "=", multiply(a, b))
+
+        #elif choice == '4':
+            #print(a, "/", b, "=", divide(a, b))
+
+         ## Uncomment this to deploy this feature    
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Let's do next calculation? (yes/no): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
